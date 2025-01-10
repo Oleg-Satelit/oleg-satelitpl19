43fef2d01e8236ea7ed974fbd2646d663fe526ca
Janis Karasa <jkarasa@gmail.com>
2025-01-10 10:35:07 +0100

    Added a Project Section with image carousel that uses SwiperJS library. Also fixed some margins and font sizes with clamp functions. And refined some code.

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,41 @@ navLinks.forEach((link) => {
   });
 });
 // toggling menu end
+
+// Initializing SwiperJS
+const swiper = new Swiper(".slider-wrapper", {
+  // Optional parameters
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+
+  autoplay: {
+    delay: 4000
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
